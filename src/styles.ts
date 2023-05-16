aaab24da1349b108c2a60582cbbfadc817cbcbbd
Edilson Bispo <edilson_pixot@hotmail.com>
2023-05-16 15:52:48 -0300

    Criando a seção de projetos e o componente de projeto

--- src/styles.ts
+++ src/styles.ts
@@ -2,19 +2,20 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
 const EstiloGlobal = createGlobalStyle`
- *{
-  margin: 0;
-  padding: 0;
-  font-family: 'Inter', sans-serif;
-}
+  *{
+    margin: 0;
+    padding: 0;
+    font-family: 'Inter', sans-serif;
+    list-style: none;
+  }
 
-body {
-  padding-top: 80px;
-}
+  body {
+      padding-top: 80px;
 
-@media (max-width: 768px) {
-  padding-top: 16px;
-}
+    @media (max-width: 768px) {
+      padding-top: 16px;
+    }
+  }
 `
 
 export default EstiloGlobal
