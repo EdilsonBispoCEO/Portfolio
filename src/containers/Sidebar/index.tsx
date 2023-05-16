c067dfed82d742efeb6d80384b04fe8a01d08658
Edilson Bispo <edilson_pixot@hotmail.com>
2023-05-16 16:53:31 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/EdilsonBispoCEO.png" />
-    <Titulo fontSize={20}>Edilson Bispo</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Edilson Bispo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        EdilsonBispoCEO
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack Python
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
