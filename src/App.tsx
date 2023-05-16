aaf804a444c29201f80341e2d60acab0ed3f01a6
Edilson Bispo <edilson_pixot@hotmail.com>
2023-05-16 15:05:04 -0300

    Construindo a estrutura e a seção "sobre mim"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import Teste from './Teste'
+// import Teste from './Teste'
+import Projetos from './containers/Projetos'
+import Sidebar from './containers/Sidebar'
+import Sobre from './containers/Sobre'
+import EstiloGlobal, { Container } from './styles'
 
 function App() {
-  return <Teste />
+  // return <Teste />
+  return (
+    <>
+      <EstiloGlobal />
+      <Container>
+        <Sidebar />
+        <main>
+          <Sobre />
+          <Projetos />
+        </main>
+      </Container>
+    </>
+  )
 }
 
 export default App
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/EdilsonBispoCEO.png" />
+    <Titulo fontSize={20}>Edilson Bispo</Titulo>
+  </aside>
+)
+
+export default Sidebar
